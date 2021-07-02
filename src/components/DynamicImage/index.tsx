a55c7c9fa50d6d2a4a7ddadb72d6b94241d5d96e
Willer Silva <willerrodrigo2006@hotmail.com>
2021-07-02 14:20:53 -0300

    feat(dynamic image): Add refetch button

--- src/components/DynamicImage/index.tsx
+++ src/components/DynamicImage/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Fontisto } from '@expo/vector-icons'
+import { BorderlessButton } from 'react-native-gesture-handler'
+
+import { queryClient } from '../../services/query-client'
 
 import Sunny from '../../assets/images/sunny-day.png'
 
 import * as S from './styles'
 
 function DynamicImage(): JSX.Element {
-  return <S.Container source={Sunny}></S.Container>
+  const handleWeatherRefetch = async (): Promise<void> => {
+    await queryClient.refetchQueries(['home'])
+  }
+
+  return (
+    <S.Container source={Sunny}>
+      <BorderlessButton onPress={handleWeatherRefetch}>
+        <Fontisto name="cloud-refresh" size={32} color="#FFFFFF" />
+      </BorderlessButton>
+    </S.Container>
+  )
 }
 
 export default DynamicImage
--- src/components/DynamicImage/styles.ts
+++ src/components/DynamicImage/styles.ts
@@ -3,4 +3,5 @@ import styled from 'styled-components/native'
 export const Container = styled.ImageBackground`
   width: 100%;
   height: 320px;
+  padding: 40px 0 0 16px;
 `
