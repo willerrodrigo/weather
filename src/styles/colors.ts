4081be616c9593a3e00439e5076713c37128b295
Willer Silva <willerrodrigo2006@hotmail.com>
2021-07-02 21:15:23 -0300

    feat(styles): Add colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+export default {
+  primary: '#FFFFFF',
+  secondary: '#0DA0EA',
+  secondaryLight: 'rgba(13, 159, 234, 0.08)',
+  tertiary: '#444444',
+  tertiaryLight: '#999999',
+
+  black: '#000000'
+}
